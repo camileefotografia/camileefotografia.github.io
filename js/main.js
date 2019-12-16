33740db099c53b81f32f10b0a18a7095669b9253
camileefotografia <58941323+camileefotografia@users.noreply.github.com>
2019-12-16 12:33:16 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 let listaproductos = [
     { sesion: 'New Born', fecha: "13/12/19 17:30" },
-    { sesion: 'Maternity', fecha: "13/12/19 17:30"},
+    { sesion: 'Maternity', fecha: "13/12/19 17:30" },
     { sesion: '1 Año', fecha: "13/12/19 17:30" },
     { sesion: 'New Born', fecha: "13/12/19 17:30" },
     { sesion: 'Navideña', fecha: "13/12/19 17:30" }
@@ -20,7 +20,7 @@ function configurarListeners() {
         let input = document.querySelector("#sesion");
         console.log(input.value);
         let producto = input.value;
-        
+
         if (producto != "") {
             console.log(producto);
             listaproductos.push({
@@ -31,10 +31,25 @@ function configurarListeners() {
         }
     })
 
-    document.querySelector("#btn-borrar-productos").addEventListener("click", e => {
-        listaproductos = [];
-        renderLista();
+    document.querySelector("#btnbusqueda").addEventListener("click", e => {
+        let input = document.querySelector("#txtbuscar");
+        //console.log(input.value);
+        let _producto = input.value;
+
+        if (_producto != "") {
+
+            listaproductos.forEach(prod => {
+                if (prod.sesion.toLowerCase() == _producto.toLowerCase()) {
+                    console.log(_producto + " Encontrado!");
+                    renderLista(_producto);
+                }
+            })
+        }
+        else {
+            renderLista();
+        }
     })
+
 }
 
 function borrarProd(index) {
@@ -54,38 +69,37 @@ function cambiarPrecio(index, e) {
     listaproductos[index].fecha = precio;
 }
 
-function renderLista() {
+function renderLista(nombre) {
+    if(nombre==undefined)
+        nombre="";
     if (iniRenderLista) {
         ul = document.createElement('ul');
         ul.classList.add('demo-list-item', 'mdl-list')
     }
 
     ul.innerHTML = '';
+
     listaproductos.forEach((prod, index) => {
+        if(nombre== "" || nombre.toLowerCase()==prod.sesion.toLowerCase()){
         ul.innerHTML += `
                 <li class="mdl-list__item">
-                <span class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored w-10" >
-                    <i class="material-icons"> photo_camera </i>
-                </span>
-                <span class="mdl-list__item-primary-content w-30 ml-item">
-                    ${prod.sesion}
-                </span>
-                <span class="mdl-list__item-primary-content w-20 ml-item">
+                    <span class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored w-10" >
+                        <i class="material-icons"> photo_camera </i>
+                    </span>
+                    <span class="mdl-list__item-primary-content w-30 ml-item ">
+                        ${prod.sesion}
+                    </span>
+                    <span class="mdl-list__item-primary-content w-30 ml-item ">
 
-                    <div class="mdl-textfield mdl-js-textfield">
-                        <textarea class="mdl-textfield__input fuenteCombo" type="text" rows= "2" id="sample-precio-${index}" >${prod.fecha}</textarea>
-                    </div>
-
-                </span>
-                <span class="mdl-list__item-primary-content w-20 ml-item md-18">
-                        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored"  onclick="borrarProd(${index})">
-                            <i class="material-icons">remove</i>
-                        </button>
-                    </button>
-                </span>
-            </li>
-    `
+                        <div class="mdl-textfield mdl-js-textfield">
+                            <textarea class="mdl-textfield__input fuenteCombo" type="text" rows= "1" id="sample-precio-${index}" >${prod.fecha}</textarea>
+                        </div>
+                    </span>
+                </li>
+                `
+        }
     })
+    
 
     let lista = document.querySelector('#lista');
     lista.appendChild(ul);
